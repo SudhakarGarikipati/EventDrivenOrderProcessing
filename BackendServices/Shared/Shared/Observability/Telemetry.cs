d4c4e7c15eedd49014819a56e115c3b082af3a9a
Garikipati SudhakaraRao <garikipati.sudhakarrao@gmail.com>
2026-04-30 20:25:40 +0530

    Introduced additional projects for new backend service for inventory.

---
+++ BackendServices/Shared/Shared/Observability/Telemetry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Observability
+{
+    public class Telemetry
+    {
+        public static readonly ActivitySource OrderActivity = new("OrderService");
+        // For PaymentService, change to "PaymentService"
+
+        public static readonly ActivitySource PaymentActivity = new("PaymentService");
+
+    }
+}
